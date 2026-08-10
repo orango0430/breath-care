7b8e3ac979db5b2885833b05c20ae65e6b86586d
choi si won <qm0430@naver.com>
2026-08-10 14:25:53 +0900

    Initial commit

---
+++ src/main/java/org/exaple/breath_care/BreathCareApplication.java
@@ -0,0 +1,13 @@
+package org.exaple.breath_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreathCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreathCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/exaple/breath_care/BreathCareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exaple.breath_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreathCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
